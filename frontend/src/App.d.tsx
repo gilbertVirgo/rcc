781cbb2aa9527ba5b577874ec3dbe501093663c5
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-11-23 15:25:30 +0000

    update

---
+++ frontend/src/App.d.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './components/Nav';
+
+import "./style/main.scss";
+
+const App = () => {
+  return (<>
+    <Nav />
+
+  </>);
+}
+
+export default App;
---
+++ frontend/src/components/Nav.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { getBreakpoints } from "../functions";
+
+const Item = ({ children }: { children: string }) => (
+    <p className="item">{children}</p>
+)
+
+const Nav = () => {
+    const [bp, setBp] = useState(getBreakpoints(window.innerWidth));
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        const resize = () => setBp(getBreakpoints(window.innerWidth));
+
+        window.addEventListener("resize", resize);
+        return () => window.removeEventListener("resize", resize);
+    }, []);
+
+    return (
+        <div className="nav">
+            <div className="bar">
+                <img
+                    className="logo"
+                    src={require("../assets/logo.webp")} />
+                <div className={`items ${active ? "active" : ""}`}>
+                    <Item>About</Item>
+                    <Item>Contact</Item>
+                </div>
+                <button
+                    className={"hamburger hamburger--squeeze " + (active ? "is-active" : "")}
+                    type="button"
+                    onClick={() => setActive(!active)}>
+                    <span className="hamburger-box">
+                        <span className="hamburger-inner"></span>
+                    </span>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
